cafae2598c17ad5cc9eacd61984854aaddcac942
Пользователь <Ilnur0939>
2022-12-01 19:27:19 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,65 @@
+// function func(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] % 2){
+//             return i
+//         }
+//     }
+    
+// }
+
+// console.log(func([1, 3, 2, 6, 4, 8,])); 
+
+
+
+
+// function func(arr){
+//     let n = ''
+//     for(let i = 0; i < arr.length; i++){
+//         n += arr[i][0]
+//     }
+//     return n
+// }
+
+// console.log(func(['Adam', 'Sarah', 'Malc'])); 
+
+
+
+
+// function func(str){
+//     let arr = str.split(' ')
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] == 'Nemo'){
+//             return `I found nemo at ${i}`
+//         }
+//     }
+//     return 'I can not find nemo :('
+// }
+
+// console.log(func('I am finding Nemo')); 
+// console.log(func('Nemo is am')); 
+// console.log(func(' is am')); 
+
+
+
+
+function func(str){
+    for(let i = 0; i < str.length; i++){
+        if((str[0] + str[1] + str[2]) == str.reverse()[0]){
+            return true
+        }
+    }
+    return false
+    
+        
+}
+
+console.log(func(['rsq', '6hi', 'g', 'rsq6hig'])); 
+console.log(func([1, 1, 1, '11'])); 
+console.log(func([8, 'thunder', true, '8thundertrue']));
+
+
+
+
+
+
+
